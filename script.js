72318af0ac340f2b86ead52c26b574a75b606e95
doing1024 <doing1024@outlook.it>
2024-08-11 13:45:45 +0800

    实现记录颜色主题

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ var converter = new showdown.Converter({
         showdownKatex({
             // 使用方式见：https://obedm503.github.io/showdown-katex
             // 详细配置见：https://katex.org/docs/options.html
-            throwOnError: false, // 公式有错时，是否抛出错误
+            throwOnError: true, // 公式有错时，是否抛出错误
             displayMode: false, // 如果为false，公式以inline方式渲染
             delimiters: [
                 { left: '$$', right: '$$', display: true },
@@ -41,7 +41,7 @@ editor.setValue(window.localStorage.getItem('lastText'));
 showbar.innerHTML = converter.makeHtml(editor.getValue());
 var todark = function (){        
     css.href = "https://unpkg.com/github-markdown-css@5.6.1/github-markdown-dark.css";
-    editor.setOption("theme", "material");
+    editor.setOption("theme", "abcdef");
     return false;
 };
 var tolight = function (){
@@ -49,11 +49,9 @@ var tolight = function (){
     editor.setOption("theme","xq-light");
     return true;
 };
-var isLight = true;
+if (window.localStorage.getItem('isLight') == 'false') document.getElementsByTagName('input')[0].checked = 1,todark();
+if (window.localStorage.getItem('isLight') == null) window.localStorage.setItem('isLight','true');
 var changeTheme = function (){
-    isLight = (isLight?todark():tolight());
+    if (window.localStorage.getItem('isLight') == 'true') window.localStorage.setItem('isLight',todark());
+    else window.localStorage.setItem('isLight',tolight());
 };
-        
-
-
-
